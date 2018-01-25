aad50124d9e226d55b4dbbec9c08f907a3231265
Khalilah Sutton <ksutton@wlapcs.org>
2018-01-25 20:27:22 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
 function setup(){
     createCanvas(600,400);
-    background(20,0,0);
+    
 }
 
 function draw(){
+    background(0);
     
+    loadPixels();
+    
+    for(var row = 0; row < height; row++){
+        for(var col = 0; col < width; col++){
+           var startingIndex = (col + row * width)*4;
+           pixels[startingIndex] = 255; //red
+           pixels[startingIndex + 1] = 110; //green
+           pixels[startingIndex + 2] = 115; //blue
+           pixels[startingIndex + 3] = 70; //transparent(alpha)
+        }
+    }
+    
+    
+    
+    updatePixels();
 }
