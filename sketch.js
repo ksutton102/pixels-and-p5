ab8fee8646e1632bffc11739260f56fd40fde1f3
Khalilah Sutton <ksutton@wlapcs.org>
2018-01-30 19:45:22 +0000

    another commit

--- sketch.js
+++ sketch.js
@@ -1,20 +1,32 @@
+var kpopUpload;
+
+function preload(){
+    kpopUpload = loadImage("kpop.jpg");
+}
+
 function setup(){
-    createCanvas(600,400);
+    createCanvas(kpopUpload.width,kpopUpload.height);
     
 }
 
 function draw(){
     background(0);
-    
+    image(kpopUpload, 0, 0);
     loadPixels();
     
     for(var row = 0; row < height; row++){
         for(var col = 0; col < width; col++){
            var startingIndex = (col + row * width)*4;
-           pixels[startingIndex] = 255; //red
-           pixels[startingIndex + 1] = 110; //green
-           pixels[startingIndex + 2] = 115; //blue
-           pixels[startingIndex + 3] = 70; //transparent(alpha)
+           
+           var r = pixels[startingIndex + 0];
+           var g = pixels[startingIndex + 1];
+           var b = pixels[startingIndex + 2];
+           var a = pixels[startingIndex + 3];
+           
+          pixels[startingIndex] = r+50; //red
+          pixels[startingIndex + 1] = g+50; //green
+          pixels[startingIndex + 2] = b+50; //blue
+          pixels[startingIndex + 3] = a; //transparent(alpha)
         }
     }
     
